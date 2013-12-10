7ec45cb011d06dfc8f95a2b379e41d3f9ffc8974
Kevin Simpson <ktsimpso@gmail.com>
2013-12-10 18:07:53 -0500

    First commit

---
+++ js/main.js
@@ -0,0 +1,105 @@
+(function () {
+	var template_utils,
+		app;
+
+	template_utils = (function () {
+		var exports = {},
+			templates = {};
+
+		exports.get = function (template_path) {
+			if (templates.hasOwnProperty(template_path)) {
+				return templates[template_path];
+			}
+
+			$.ajax(template_path, {
+				async: false,
+				success: function (data) {
+					templates[template_path] = Mustache.compile(data);
+				}
+			});
+
+			if (templates.hasOwnProperty(template_path)) {
+				return templates[template_path];
+			}
+
+			throw 'Template: ' + template_path + ' not found'; 
+		};
+
+		return exports;
+	}());
+
+	app = new Backbone.Marionette.Application();
+
+	app.addRegions({
+		content: '.content'
+	});
+
+	app.addInitializer(function () {
+		var that = this,
+			origin = window.location.origin,
+			success;
+
+		this.router = new Backbone.Router();
+		this.views = {};
+
+		$(document).on('click', 'a', function (e) {
+			var self = $(this),
+				href = self.attr('href');
+
+			e.preventDefault();
+			
+			that.router.navigate(href, {
+				trigger: true
+			});
+		});
+
+		this.views.Home = Marionette.View.extend({
+			template: template_utils.get(origin + '/templates/home.mustache'),
+			render: function () {
+				this.$el.html(this.template({}));
+			} 
+		});
+
+		this.router.route('', 'home', function () {
+			var home_view = new that.views.Home();
+			that.content.show(home_view);
+		});
+
+		this.views.Test = Marionette.View.extend({
+			template: template_utils.get(origin + '/templates/test.mustache'),
+			render: function () {
+				this.$el.html(this.template({}));
+			}
+		});
+
+		this.router.route('test', 'test', function () {
+			var test_view = new that.views.Test();
+			that.content.show(test_view);
+		});
+
+		this.views.NotFound = Marionette.View.extend({
+			template: template_utils.get(origin + '/templates/404.mustache'),
+			render: function () {
+				this.$el.html(this.template({}));
+			}
+		});
+
+		this.router.route('404', '404', function () {
+			var not_found_view = new that.views.NotFound();
+			that.content.show(not_found_view);
+		});
+
+		success = Backbone.history.start({
+			pushState: true
+		});
+
+		if (!success) {
+			this.router.navigate('404', {
+				trigger: true,
+				replace: true
+			});
+		}
+	});
+
+	app.start({});
+}());
---
+++ server.js
@@ -0,0 +1,73 @@
+var express = require('express'),
+	mustache = require('mustache'),
+	fs = require('fs'),
+	server = express(),
+	statics = {
+		'js': true,
+		'templates': true,
+		'css': true,
+		'favicon.ico': true
+	},
+	template_utils;
+
+template_utils = (function () {
+	var exports = {},
+		templates = {};
+
+	exports.get = function (template_path) {
+		if (templates.hasOwnProperty(template_path)) {
+			return templates[template_path];
+		}
+
+		templates[template_path] = fs.readFileSync(template_path).toString();
+		mustache.parse(templates[template_path]);
+
+		return templates[template_path];
+	};
+
+	return exports;
+}());
+
+template_utils.get('index.html');
+
+server.get('*', function (request, response) {
+	var urls = request.params[0].split('/'),
+		static_url = '';
+	
+	//remove leading slash
+	urls.shift();
+
+	//remove trailing slash if provided
+	if (urls[urls.length - 1] === '') {
+		urls.pop();
+	}
+
+	if (statics[urls[0]]) {
+		console.log('Serving static file: ' + urls.join('/'));
+		response.sendfile(urls.join('/'));
+		return;
+	}
+
+	urls.forEach(function () {
+		static_url += '../';
+	});
+
+	console.log('Serving index.html');
+	response.send(mustache.render(template_utils.get('index.html'), {
+		static_url: static_url
+	}));
+});
+
+server.use(function(error, request, response, next) {
+	if (error.status === 404) {
+		console.log('No file found');
+		response.send(404, 'No such file!');
+		return;
+	}
+
+	console.log('Error!: ' + error.stack);
+	response.send(500, 'Internal Server Error');
+});
+
+server.listen(8000);
+console.log('Listening on port 8000');
