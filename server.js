e7c699aa451002257a9b26ece721c7130599e1b7
Kevin Simpson <ktsimpso@gmail.com>
2013-12-23 17:35:16 -0500

    JS files are now combined into one file on server start. Cleaned up unsused files in the generated tmp directory.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express'),
 	fse = require('fs-extra'),
 	hogan = require('hogan.js'),
+	requirejs = require('requirejs'),
 	server = express(),
 	statics = {
 		'js': true,
@@ -10,7 +11,10 @@ var express = require('express'),
 		'robots.txt': true
 	},
 	output_directory = 'tmp',
+	require_config,
 	index_template;
+
+fse.deleteSync(output_directory);
 
 //Copying files to temp directory
 Object.keys(statics).forEach(function (static_file) {
@@ -30,40 +34,63 @@ fse.readdirSync(output_directory + '/templates').forEach(function (template) {
 	fse.outputFileSync(js_file_name, js_file);
 });
 
-index_template = hogan.compile(fse.readFileSync('index.html').toString());
+//Removing templates directory
+fse.deleteSync(output_directory + '/templates');
+delete statics['templates'];
 
-server.get('*', function (request, response) {
-	var urls = request.params[0].split('/');
-	
-	//remove leading slash
-	urls.shift();
+require_config = {
+	baseUrl: output_directory + '/js',
+	name: 'almond',
+	include: ['main'],
+	mainConfigFile: output_directory + '/js/main.js',
+	out: output_directory + '/tmp/main.js',
+	wrap: false
+}
 
-	//remove trailing slash if provided
-	if (urls[urls.length - 1] === '') {
-		urls.pop();
-	}
+requirejs.optimize(require_config, function (modules) {
+	//Clean up optimizer
+	fse.deleteSync(output_directory + '/js');
+	fse.copySync(output_directory + '/tmp/main.js', output_directory + '/js/main.js');
+	fse.deleteSync(output_directory + '/tmp');
 
-	if (statics[urls[0]]) {
-		console.log('Serving static file: ' + urls.join('/'));
-		urls.unshift(output_directory);
-		response.sendfile(urls.join('/'));
-		return;
-	}
+	console.log('Success: ' + modules);
+	index_template = hogan.compile(fse.readFileSync('index.html').toString());
 
-	console.log('Serving index.html');
-	response.send(index_template.render({}));
+	server.get('*', function (request, response) {
+		var urls = request.params[0].split('/');
+
+		//remove leading slash
+		urls.shift();
+
+		//remove trailing slash if provided
+		if (urls[urls.length - 1] === '') {
+			urls.pop();
+		}
+
+		if (statics[urls[0]]) {
+			console.log('Serving static file: ' + urls.join('/'));
+			urls.unshift(output_directory);
+			response.sendfile(urls.join('/'));
+			return;
+		}
+
+		console.log('Serving index.html');
+		response.send(index_template.render({}));
+	});
+
+	server.use(function(error, request, response, next) {
+		if (error.status === 404) {
+			console.log('No file found');
+			response.send(404, 'No such file!');
+			return;
+		}
+
+		console.log('Error!: ' + error.stack);
+		response.send(500, 'Internal Server Error');
+	});
+
+	server.listen(8000);
+	console.log('Listening on port 8000');
+}, function (error) {
+	console.log('Error: ' + error);
 });
-
-server.use(function(error, request, response, next) {
-	if (error.status === 404) {
-		console.log('No file found');
-		response.send(404, 'No such file!');
-		return;
-	}
-
-	console.log('Error!: ' + error.stack);
-	response.send(500, 'Internal Server Error');
-});
-
-server.listen(8000);
-console.log('Listening on port 8000');
