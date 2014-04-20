cbccfe6d4e9b2aa1d4f1e738f6c5dec5ad5e56fb
Kevin Simpson <ktsimpso@gmail.com>
2014-04-19 20:57:18 -0400

    Refactored client routes.

--- js/routes.js
+++ js/routes.js
@@ -1,41 +1,38 @@
 define(function (require, exports, module) {
 	var views = require('views'),
 		app = require('app'),
-		home = require('routes/home'),
-		test = require('routes/test'),
-		not_found = require('routes/404'),
-		var_test = require('routes/var');
+		routes = [];
+
+	routes.push(require('routes/home'));
+	routes.push(require('routes/test'));
+	routes.push(require('routes/404'));
+	routes.push(require('routes/var'));
 
 	exports.router = new Backbone.Router();
 
-	exports.Route = function (path, name, View, argument_names) {
-		if (!argument_names) {
-			argument_names = [];
+	routes.forEach(function (route) {
+		if (!route.argument_names) {
+			route.argument_names = [];
 		}
 
-		exports.router.route(path, name, function () {
+		exports.router.route(route.path, route.name, function () {
 			var data = {
 					data: {}
 				},
 				args = Array.prototype.slice.call(arguments),
 				view;
 
-			if (args.length !== argument_names.length) {
-				throw new Error('Expected ' + argument_names.length + ' arguments but got ' + args.length);
+			if (args.length !== route.argument_names.length) {
+				throw new Error('Expected ' + route.argument_names.length + ' arguments but got ' + args.length);
 			}
 
 			args.forEach(function (argument, index) {
-				data.data[argument_names[index]] = argument;
+				data.data[route.argument_names[index]] = argument;
 			});
 
-			view = new View(data);
+			view = new views[route.view](data);
 
 			app.content.show(view);
 		});
-	};
-
-	exports.Route(home.path, home.name, views[home.view]);
-	exports.Route(test.path, test.name, views[test.view]);
-	exports.Route(not_found.path, not_found.name, views[not_found.view]);
-	exports.Route(var_test.path, var_test.name, views[var_test.view], var_test.argument_names);
+	});
 });
